e295bb3ca979196f1da8fc59b4ed8d5e808069cd
mhmdhaekal <hkalipaksi@proton.me>
2025-07-05 21:09:28 +0700

    improve workflow

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,18 +1,18 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.colorcolumn = "100"
-vim.opt.textwidth = 100
+vim.g.mapleader          = " "
+vim.g.maplocalleader     = " "
+vim.g.have_nerd_font     = true
+vim.opt.number           = true
+vim.opt.relativenumber   = true
+vim.opt.mouse            = "a"
+vim.opt.showmode         = false
+vim.opt.tabstop          = 2
+vim.opt.shiftwidth       = 2
+vim.opt.colorcolumn      = "100"
+vim.opt.textwidth        = 100
 
-vim.g.loaded_netrw = 0
-vim.g.loaded_netrwPlugin = 0
-vim.opt.termguicolors = true
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors    = true
 
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
@@ -43,3 +43,5 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Buffer next" })
 vim.keymap.set("n", "<S-Tab>", ":bprev<CR>", { desc = "Buffer next" })
+
+vim.api.nvim_create_user_command("W", "w", { desc = "Write" })
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -7,4 +7,29 @@ vim.pack.add({
 	},
 })
 
-require("nvim-tree").setup({})
+
+require('nvim-tree').setup({
+	view = {
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get()
+				local window_w = 30
+				local window_h = screen_h - 4
+
+				return {
+					border = 'rounded',
+					relative = 'editor',
+					row = 0,
+					col = screen_w - window_w, -- Position at right edge
+					width = window_w,
+					height = window_h,
+				}
+			end,
+		},
+		width = {
+			max = -1
+		},
+	}
+})
